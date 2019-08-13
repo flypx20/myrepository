546ce256f10a3b444904b756f0402e07d88895d9
flypx20 <1728925053@qq.com>
2019-08-13 17:14:04 +0800

    vue for test

---
+++ vue组件通信/component/father/father.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <!-- <com-article bind-articles = "articlelist"></com-article> -->
+    </div>
+</template>
+<script>
+export default {
+    name:"father",
+    data(){
+        return {
+            articlelist:['红楼梦','西游计','三国演义']
+        }
+    }
+}
+</script>
